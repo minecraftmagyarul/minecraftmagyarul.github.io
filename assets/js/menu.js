38219a709fb6c4ba4ebb041761ba8eb0709adc87
Marton Huszics <Marton.Huszics@ocean.ibm.com>
2023-03-06 10:56:57 +0100

    init

---
+++ assets/js/menu.js
@@ -0,0 +1,31 @@
+$(window).on("scroll", function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        $("#site-header").addClass("nav-fixed");
+    } else {
+        $("#site-header").removeClass("nav-fixed");
+    }
+});
+
+//Main navigation Active Class Add Remove
+$(".navbar-toggler").on("click", function() {
+    $("header").toggleClass("active");
+});
+$(document).on("ready", function() {
+    if ($(window).width() > 991) {
+        $("header").removeClass("active");
+    }
+    $(window).on("resize", function() {
+        if ($(window).width() > 991) {
+            $("header").removeClass("active");
+        }
+    });
+});
+
+// disable body scroll which navbar is in active
+$(function() {
+    $('.navbar-toggler').click(function() {
+        $('body').toggleClass('noscroll');
+    })
+});
